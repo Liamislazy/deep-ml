f9a46280eab522352f08934933d04af3e36375a8
Liamislazy <159439509+Liamislazy@users.noreply.github.com>
2026-09-21 03:37:33 +0000

    Solve: Dropout Layer

---
+++ problems/0151-dropout-layer/solution.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+class DropoutLayer:
+    def __init__(self, p: float):
+        """Initialize the dropout layer.
+        
+        Attributes to set:
+            self.p: the dropout rate
+            self.mask: stores the dropout mask (initially None)
+        """
+        self.p = p
+        self.mask = None
+
+    def forward(self, x: np.ndarray, training: bool = True) -> np.ndarray:
+        """Forward pass of the dropout layer.
+        
+        Generate a new mask on each training forward pass and store it in self.mask.
+        """
+        if not training: return x
+        self.mask = np.random.binomial(n=1, p=1-self.p, size=x.shape)
+        return np.array((x * self.mask) / (1 - self.p))
+
+    def backward(self, grad: np.ndarray) -> np.ndarray:
+        """Backward pass of the dropout layer.
+        
+        Use the stored self.mask from the most recent forward pass.
+        """
+        grad = (grad * self.mask) / (1- self.p)
+        return grad
